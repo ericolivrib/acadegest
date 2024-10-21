7247b3fcca6f25b2dca3dd1bafcd4813af5d814c
Érico <oliv.ericorib@gmail.com>
2024-10-21 12:11:43 -0300

    feat: iniciar repositório

---
+++ src/main/java/br/ufsm/redescomp/acadegest/AcadegestApplication.java
@@ -0,0 +1,13 @@
+package br.ufsm.redescomp.acadegest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcadegestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcadegestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufsm/redescomp/acadegest/AcadegestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufsm.redescomp.acadegest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcadegestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
